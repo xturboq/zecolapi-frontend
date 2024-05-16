46454e64a69e9959d74897eb77221b0726cdb209
zecola <xyk816@gmail.com>
2024-05-16 10:35:41 +0800

    接口文档页

--- src/pages/Index/index.tsx
+++ src/pages/Index/index.tsx
@@ -1,10 +1,66 @@
-import {PageContainer} from '@ant-design/pro-components';
+import { PageContainer } from '@ant-design/pro-components';
+import React, { useEffect, useState } from 'react';
+import { List, message } from 'antd';
+import {listInterfaceInfoByPageUsingGet} from "@/services/zecolapi-backend/interfaceInfoController";
 
-
+/**
+ * 主页
+ * @constructor
+ */
 const Index: React.FC = () => {
+  const [loading, setLoading] = useState(false);
+  const [list, setList] = useState<API.InterfaceInfo[]>([]);
+  const [total, setTotal] = useState<number>(0);
+
+  const loadData = async (current = 1, pageSize = 5) => {
+    setLoading(true);
+    try {
+      const res = await listInterfaceInfoByPageUsingGet({
+        current,
+        pageSize,
+      });
+      setList(res?.data?.records ?? []);
+      setTotal(res?.data?.total ?? 0);
+    } catch (error: any) {
+      message.error('请求失败，' + error.message);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
   return (
-    <PageContainer title = "在线接口开发平台">
-      阿爸阿爸
+    <PageContainer title="在线接口开放平台">
+      <List
+        className="my-list"
+        loading={loading}
+        itemLayout="horizontal"
+        dataSource={list}
+        renderItem={(item) => {
+          const apiLink = `/interface_info/${item.id}`;
+          return (
+            <List.Item actions={[<a key={item.id} href={apiLink}>查看</a>]}>
+              <List.Item.Meta
+                title={<a href={apiLink}>{item.name}</a>}
+                description={item.description}
+              />
+            </List.Item>
+          );
+        }}
+        pagination={{
+          // eslint-disable-next-line @typescript-eslint/no-shadow
+          showTotal(total: number) {
+            return '总计：' + total;
+          },
+          pageSize: 5,
+          total,
+          onChange(page, pageSize) {
+            loadData(page, pageSize);
+          },
+        }}
+      />
     </PageContainer>
   );
 };
